4f2ba52f5da95290a60210f6a1af30b282f11e41
Ivan Prikaznov <77636647+Prikaz98@users.noreply.github.com>
2025-04-16 15:11:30 +0200

    base functionality

--- src/cljs/ivankytype_ui/core.cljs
+++ src/cljs/ivankytype_ui/core.cljs
@@ -1,1 +1,71 @@
-(ns ivankytype-ui.core)
+(ns ivankytype-ui.core
+  (:require [clojure.string :refer [blank?]]))
+
+;;TODO: Store metrics of all clicks and draw graph
+;;TODO: Add animation when restart
+
+(def input (.getElementById js/document "input"))
+(def txt (.getElementById js/document "txt"))
+(def done (.getElementById js/document "done"))
+(def stat (.getElementById js/document "stat"))
+(def sizes (.getElementById js/document "sizes"))
+(def warn (.getElementById js/document "warn"))
+(def restart (.getElementById js/document "restart"))
+
+(def start-time (atom nil))
+(def finished-time (atom nil))
+(def attempts (atom 0))
+
+(defn floor [num scale]
+  (let [multip (js/Math.pow 10 scale)]
+    (/ (js/Math.round (* num multip)) multip)))
+
+(defn fill-stat []
+  (let [seconds (if @start-time (floor (/ (- (or @finished-time (js/Date.now)) @start-time) 1000) 2) 0)]
+    (set!
+     (.-innerText stat)
+     (str seconds " seconds" " / " @attempts " attempts"))))
+
+(defn input-listener-keydown [event]
+  (when (nil? @start-time)
+    (reset! start-time (js/Date.now)))
+
+  (let [key (.-key event)]
+    (when (= (count key) 1)
+      (let [content (.-innerText txt)
+            head (nth content 0)]
+        (if (= head key)
+          (do
+            (set! (.-innerText done) (str (.-innerText done) head))
+            (when (blank? (set! (.-innerText txt) (.substring content 1)))
+              (reset! finished-time (js/Date.now))))
+            (do
+              (swap! attempts #(+ 1 %))
+              (set! (.-innerText txt) (str (.-innerText done) (.-innerText txt)))
+              (set! (.-innerText done) "")))))))
+
+(defn input-listener-focusout [event]
+  (set! (.-innerText warn) "Click on text to continue"))
+
+(defn txt-listener-click [event]
+  (set! (.-innerText warn) "")
+  (.focus input))
+
+(defn restart-listener-click [event]
+  (.reload js/location))
+
+(.addEventListener input "keydown" input-listener-keydown)
+(.addEventListener input "focusout" input-listener-focusout)
+(.addEventListener txt "click" txt-listener-click)
+(.addEventListener restart "click" restart-listener-click)
+
+(fill-stat)
+(js/setInterval fill-stat 100)
+
+(doseq [button (.-children sizes)]
+  (let [value (.-innerText button)]
+    (.addEventListener
+     button
+     "click"
+     (fn [_]
+       (.replace js/location (str (.-url js/document) "?size=" value))))))
--- src/ivankytype/view/main.clj
+++ src/ivankytype/view/main.clj
@@ -7,15 +7,47 @@
 
 (defn- index-page [size]
   (html [:html
+
          [:head
           [:title "Ivankytype"]
+          [:link {:href "https://pvinis.github.io/iosevka-webfont/3.4.1/iosevka.css"
+                  :rel "stylesheet"}]
           [:link {:href "css/style.css"
                   :rel "stylesheet"
                   :type "text/css"}]]
          [:body
+          [:div {:class "settings"}
+           [:div {:id "sizes"}
+            [:button (when (and size (= size 10)) {:class "active"}) "10"]
+            [:button (when (and size (= size 25)) {:class "active"}) "25"]
+            [:button (when (and size (= size 50)) {:class "active"}) "50"]
+            [:button (when (and size (= size 100)) {:class "active"}) "100"]]]
+          [:div {:class "info"}
+           [:div {:style "float: right;"}
+            [:label {:id "stat"}]]
+           [:p
+            [:a {:href "https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki"}
+             "BIP-39"]
+            " Typing trainer"]]
+          [:div {:class "tofill"}
+           [:p
+            [:label {:id "done"} ""]
+            [:label {:id "txt"
+                     :class "unfill"}
+             (gen/get-text size)]]]
+
+          [:label {:id "warn"
+                   :class "warn"}]
+
+          [:button {:id "restart"
+                    :class "restart-btn"}
+           [:img {:src "images/restart.png"}]]
+
+          [:textarea {:id "input"
+                        :autofocus true
+                        :class "hide"}]
           [:script {:src "js/compiled/ivankytype.js"
-                    :type "text/javascript"}]
-          [:p (gen/get-text size)]]]))
+                    :type "text/javascript"}]]]))
 
 
 (defn index [size]
@@ -23,7 +55,6 @@
       (str)
       (response)
       (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-
 
 (comment
   (index 100)
